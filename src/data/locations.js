0a2f3da52ad0b9b12713b58f1e9146896d9dd4bb
katnguyen2301-ops <katnguyen2301@gmail.com>
2026-07-30 15:01:51 +1000

    Update locations.js

--- src/data/locations.js
+++ src/data/locations.js
@@ -1,27 +1,3 @@
-$ cat << 'ENDOFFILE'
-const locations = [
-  {
-    slug: 'eyespecs-cbd',
-    name: 'Eyespecs Optometry',
-    tagline: 'Eye Care & Eyewear Clinic',
-    phone: '(02) 9725 6635',
-    email: 'info@eyespecs.example',
-    address: '10 Arthur St, Cabramatta NSW 2166',
-    hours: [
-      { day: 'Monday', time: '9:30 am - 5:15 pm' },
-      { day: 'Tuesday', time: '9:30 am - 5:15 pm' },
-      { day: 'Wednesday', time: '10:00 am - 2:00 pm' },
-      { day: 'Thursday', time: '9:30 am - 5:15 pm' },
-      { day: 'Friday', time: '9:30 am - 5:15 pm' },
-      { day: 'Saturday', time: '9:45 am - 4:00 pm' },
-      { day: 'Sunday', time: 'Closed' },
-    ],
-  },
-]
-
-export default locations
-ENDOFFILE
-
 const locations = [
   {
     slug: 'eyespecs-cbd',
