f242eddba0d63200c98042474204853e22ac4b32
katnguyen2301-ops <katnguyen2301@gmail.com>
2026-07-30 15:02:13 +1000

    Update SocialLinks.jsx

--- src/SocialLinks.jsx
+++ src/SocialLinks.jsx
@@ -1,24 +1,3 @@
-$ cat << 'ENDOFFILE'
-function SocialLinks({ className }) {
-  return (
-    <div className={className ? `social-links ${className}` : 'social-links'}>
-      <a
-        href="https://www.facebook.com/DianeNguyenOptom/"
-        target="_blank"
-        rel="noreferrer"
-        aria-label="Eyespecs on Facebook"
-      >
-        <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-          <path d="M22 12.06C22 6.505 17.523 2 12 2S2 6.505 2 12.06c0 5.02 3.657 9.184 8.438 9.94v-7.03H7.898v-2.91h2.54V9.845c0-2.522 1.492-3.915 3.777-3.915 1.094 0 2.238.196 2.238.196v2.475h-1.26c-1.243 0-1.63.775-1.63 1.57v1.888h2.773l-.443 2.91h-2.33V22c4.78-.756 8.437-4.92 8.437-9.94Z" />
-        </svg>
-      </a>
-    </div>
-  )
-}
-
-export default SocialLinks
-ENDOFFILE
-
 function SocialLinks({ className }) {
   return (
     <div className={className ? `social-links ${className}` : 'social-links'}>
