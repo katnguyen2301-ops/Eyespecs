9392003649c487ddd15791ca7fdf688c8999c500
katnguyen2301-ops <katnguyen2301@gmail.com>
2026-08-05 11:52:51 +1000

    Update serviceImages.js

--- src/data/serviceImages.js
+++ src/data/serviceImages.js
@@ -1,15 +1,23 @@
 import comprehensiveEyeExams from '../assets/services/comprehensive-eye-exams.png'
 import childrensEyeCare from '../assets/services/childrens-eye-care.png'
+import orthoKContactLenses from '../assets/services/ortho-k-contact-lenses.png'
 import contactLensFittings from '../assets/services/contact-lens-fittings.png'
 import glaucomaScreening from '../assets/services/glaucoma-screening.png'
 import prescriptionEyewear from '../assets/services/prescription-eyewear.png'
+import dryEyeTreatment from '../assets/services/dry-eye-treatment.png'
+import diabeticEyeExams from '../assets/services/diabetic-eye-exams.png'
+import emergencyEyeCare from '../assets/services/emergency-eye-care.png'
 
 const serviceImages = {
   'comprehensive-eye-exams': comprehensiveEyeExams,
   'childrens-eye-care': childrensEyeCare,
+  'ortho-k-contact-lenses': orthoKContactLenses,
   'contact-lens-fittings': contactLensFittings,
   'glaucoma-screening': glaucomaScreening,
   'prescription-eyewear': prescriptionEyewear,
+  'dry-eye-treatment': dryEyeTreatment,
+  'diabetic-eye-exams': diabeticEyeExams,
+  'emergency-eye-care': emergencyEyeCare,
 }
 
 export default serviceImages
